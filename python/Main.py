dc703c7598a504fd9ba9f9c48909db772c262df3
Sikevux <sikevux@sikevux.se>
2011-06-11 12:55:30 +0200

    Updated license

--- python/Main.py
+++ python/Main.py
@@ -7,13 +7,13 @@
 # Common Development and Distribution License (the "License").
 # You may not use this file except in compliance with the License.
 #
-# You can obtain a copy of the license in the CDDL_LICENSE file
+# You can obtain a copy of the license in the LICENSE.CDDL file
 # or at http://dev.sikevux.se/LICENSE.txt
 # See the License for the specific language governing permissions
 # and limitations under the License.
 #
 # When distributing Covered Code, include this CDDL HEADER in each
-# file and include the License file CDDL_LICENSE.
+# file and include the License file LICENSE.CDDL
 # If applicable, add the following below this CDDL HEADER, with the
 # fields enclosed by brackets "[]" replaced with your own identifying
 # information: Portions Copyright [yyyy] [name of copyright owner]
